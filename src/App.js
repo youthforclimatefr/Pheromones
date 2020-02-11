102a3ce8b48d006ad21629543009ee00a89bb5b4
Robin Jullian <ro4100ju@simsu.fr>
2020-02-11 14:28:58 +0100

    improve auth ui and add matrix sdk in code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import * as matrix from 'matrix-js-sdk'
-
 import ServerBar from './components/ServersBar'
 import LeftPanel from './components/LeftPanel'
 import ChatPanel from './components/ChatPanel'
 import RightPanel from './components/RightPanel'
 import Auth from './components/Auth'
 
-window.client = matrix.createClient('https://matrix.org')
-
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Switch>
           <Route path="/app">
--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
-//import { Link } from "react-router-dom"
+import { useHistory } from 'react-router-dom'
 import './index.css'
 
 class Auth extends Component {
+  login() {
+    const username = ''
+    const password = ''
+  
+    window.client.login('m.login.password', {
+      user: username,
+      password: password
+    }).then(response => {
+      console.log(response)
+      console.log(response)
+      document.querySelector('.auth-loading').style.display = 'block'
+      this.props.history.push('/app')
+    })
+  }
+
   render() {
     return (
       <div className="auth">
+        <div className="auth-loading">
+          <img src="/loader.svg" />
+        </div>
         <div className="auth-form">
           <img src="/logo.png" />
 
-          <input type="text" className="input input-text" placeholder="username" />
-          <input type="password" className="input input-text" placeholder="password" />
+          <div className="auth-form-inputs">
+            <input type="text" className="input input-text" placeholder="username" />
+            <input type="password" className="input input-text" placeholder="password" />
+          </div>
 
-          <a href="#" className="input input-btn" >LOG-IN</a>
+          <a href="#" className="input input-btn" onClick={this.login.bind()} >LOG-IN</a>
         </div>
       </div>
     )
